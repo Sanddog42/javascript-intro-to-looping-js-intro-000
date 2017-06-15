7b9dda136f103a2aeb3fa027600212ae593acde8
Todd <sanddog42@hotmail.com>
2017-06-14 19:59:01 -0700

    functions declared

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+    if (i==1) {
+      array.push ("I am " + i + " strange loop.")
+    } else {
+      array.push ("I am " + i + " strange loops.")
+    }
+  }
+    return array;
+}
+
+function whileLoop(n) {
+  while (true) {
+    return n;
+  }
+}
+
+function doWhileLoop(array) {
+  
+}
